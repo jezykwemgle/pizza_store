66157f54ef0aba4fedf3dca32f00f7c51cb48b9c
jezykwemgle <96741811+jezykwemgle@users.noreply.github.com>
2023-04-21 09:36:49 +0300

    added methods and logical changes (get_pizza_by_id, lower_price, pizza_category, print_pizzas)

--- src/service/service.py
+++ src/service/service.py
@@ -1,22 +1,48 @@
-from repository.repository import PizzaRepository
+from repositor.repository import PizzaRepository
+from helpers.exeptions import MyException
+
 
 class PizzaService:
 
     def __init__(self):
         self.__repository = PizzaRepository()
 
-    def get_pizza_by_id(self, pizza_id):
-        return self.__repository.get_by_id(pizza_id)
+    def get_pizza_by_id(self):
+        try:
+            choice = int(input('Enter pizza id : '))
+            result = self.__repository.get_by_id(choice)
+            if result is not None:
+                return result
+            elif result is None:
+                raise MyException('Wrong id')
+        except MyException:
+            print('Please try again')
+            self.get_pizza_by_id()
+
+    def get_all_pizzas(self):
+        return self.__repository.get_all_pizzas()
 
     # def delete_pizza(self, pizza_id):
     #     pizza = self.__repository.get_by_id(pizza_id)
     #     pizza.isDeleted = True
     #     self.__repository.update_pizza(pizza)
 
-    def lower_price(self, price):
+    def lower_price(self):
         pizzas = self.__repository.get_all_pizzas()
+        price = int(input('Enter the upper limit:'))
         return [pizza for pizza in pizzas if pizza.price <= price]
 
-    def pizza_category(self, category):
+    def pizza_category(self):
         pizzas = self.__repository.get_all_pizzas()
+        category = input('Enter category. Available are classic, vegetarian, fishy, vegan:')
         return [pizza for pizza in pizzas if pizza.category == category]
+
+    @staticmethod
+    def print_pizzas(pizzas):
+        for i in pizzas:
+            print(i)
+
+if __name__ == '__main__':
+    s = PizzaService()
+    for i in s.get_all_pizzas():
+        print(i)
