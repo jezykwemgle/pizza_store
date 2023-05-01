118cbe74db31f6d79241bd36f08f7c554db1e9ea
jezykwemgle <96741811+jezykwemgle@users.noreply.github.com>
2023-05-01 22:50:48 +0300

    added set_data_csv, get_data_csv, from_class_to_list

--- src/helpers/helpers.py
+++ src/helpers/helpers.py
@@ -1,26 +1,3 @@
-# для роботи з json
-# треба прочитати як працювати з файлами
-# продумати як краще: ініціалізувати при створені об'єкта меню чи одразу в кожному методі працювати з джейсоном'
-
-lst = [
-    (1, 'Margarita', 100, 'Tomato + cheese', 'vegetarian'),
-    (2, 'Vege', 170, 'Tomato + Vege cheese + Paprica', 'vegetarian'),
-    (3, 'Pizza Classic', 160, 'Tomato + Ham + Cheese + Parsley', 'classic'),
-    (4, 'Meat boom', 200, 'Salami + Ham + Bacon + Veal sous vide + Mozzarella cheese + Onions', 'classic'),
-    (5, 'Pepperoni', 180, 'Pepperoni + Mozzarella Cheese + Chili pepper', 'classic'),
-    (6, 'Lorenz', 160, 'Veal sous vide + Chicken sous-vide + Mozzarella cheese + Olives + Champions + '
-                            'Sweet pepper', 'classic'),
-    (7, 'Nautical', 205, 'Cocktail shrimps + Squid + Mussels + Octopus + Mozzarella cheese + Cream',
-          'fishy'),
-    (8, 'Alpine', 190, 'Smoked chicken + Mozzarella cheese + Champions + Parsley', 'classic'),
-    (9, 'El Toro', 200, 'Veal sous vide + Mozzarella cheese + Champions + Sun-dried tomatoes + '
-                             'Blue onions', 'classic'),
-    (10, 'Vegan Bolognese pizza', 180, 'Vegetable Meat Eat Me At + Thyme + Tomatoes + Garlic + Onions', 'vegan')
-]
-
-
-
-
 import csv
 from repositor.pizza import Pizza
 
@@ -42,7 +19,17 @@ def get_data_csv(file_name):
             lst.append(pizza)
     return lst
 
-print_pizzas(get_data_csv('pizzas.csv'))
+def from_class_to_list(list_cl):
+    pizzas = []
+    for item in list_cl:
+        pizzas.append([int(item.pizza_id), item.name, int(item.price), item.ingredients, item.category])
+    return pizzas
+
+if __name__ == '__main__':
+    print_pizzas(get_data_csv('../repositor/pizzas.csv'))
+
+
+
 
 
 
